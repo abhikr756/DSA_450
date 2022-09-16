4fa943f0f357bd7af1336a32af342ae8ebe89144
Abhishek <abhikr756@gmail.com>
2022-09-16 23:41:07 +0530

    Problem 1

---
+++ Geeks For Geeks/1_find_minimum_and_maximum_in_an_array.java
@@ -0,0 +1,32 @@
+/*
+ class pair  
+{  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+} */
+class Compute 
+{
+    static pair getMinMax(long a[], long n)  
+    {
+        //Write your code here
+        long min=Long.MAX_VALUE;
+        long max=Long.MIN_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            if(min>a[i])
+            {
+                min=a[i];
+            }
+            if(max<a[i])
+            {
+                max=a[i];
+            }
+        }
+        pair ans=new pair(min,max);
+        return ans;
+    }
+}
